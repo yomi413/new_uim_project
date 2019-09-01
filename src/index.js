811d820cbae042f98a762e2e7771c100a3079e11
github username <github email address>
2019-08-31 22:16:11 -0400

    Added new imports and routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Home from "./components/Home";
 import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import StudentPage from "./components/StudentPage";
+import ParentPage from "./components/ParentPage";
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(
+  //   <Provider store={store}>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/student-page" component={StudentPage} />
+      <Route exact path="/parent-page" component={ParentPage} />
+    </Switch>
+  </Router>,
+  //   </Provider>,
+
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
