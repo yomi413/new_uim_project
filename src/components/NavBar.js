333a2e65479bee7b3fe9025d623fd73804fa86e3
github username <github email address>
2019-08-31 22:03:49 -0400

    Reviewed and revised the <NavBar /> for easy navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,122 +3,21 @@ import { Navbar, Nav } from "react-bootstrap";
 
 class NavBar extends Component {
   render() {
-    const url = window.location.pathname;
-    if (url === "/") {
-      return (
-        <div className="navbar-container">
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/login">Login</Nav.Link>
-                <Nav.Link href="/signup">Sign Up</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-      );
-    } else if (url === "/login") {
-      return (
-        <div className="navbar-container">
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/signup">Sign Up</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-      );
-    } else if (url === "/signup") {
-      return (
-        <div className="navbar-container">
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/login">Login</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-      );
-    } else if (url === "/user-welcome") {
-      return (
-        <div className="navbar-container">
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/building/new">New Building</Nav.Link>
-                <Nav.Link href="/logout">Logout</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-      );
-    } else if (url === "/building/new") {
-      return (
-        <div className="navbar-container">
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/user-welcome">All Buildings</Nav.Link>
-                <Nav.Link href="/logout">Logout</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-      );
-    } else if (/building\/\d+$/.test(url)) {
-      return (
-        <div className="navbar-container">
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/building/new">New Building</Nav.Link>
-                <Nav.Link href="/user-welcome">All Buildings</Nav.Link>
-                <Nav.Link href="/logout">Logout</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-      );
-    } else if (url === "/logout") {
-      return (
-        <div className="navbar-container">
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/building/new">New Building</Nav.Link>
-                <Nav.Link href="/user-welcome">All Buildings</Nav.Link>
-                <Nav.Link href="/logout">Logout</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-      );
-    } else if (/building\/\d+\/edit$/.test(url)) {
-      return (
-        <div className="navbar-container">
-          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/building/new">New Building</Nav.Link>
-                <Nav.Link href="/user-welcome">All Buildings</Nav.Link>
-                <Nav.Link href="/logout">Logout</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </div>
-      );
-    }
+    return (
+      <div className="navbar-container">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/parent-page">Parents</Nav.Link>
+              <Nav.Link href="/student-page">Students</Nav.Link>
+              <Nav.Link href="/signup">Senior Information</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
   }
 }
 
