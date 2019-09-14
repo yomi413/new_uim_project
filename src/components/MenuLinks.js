416c86a8b99ab90e7926da5ab88a311b9741bb4c
github username <github email address>
2019-09-14 01:23:15 -0400

    Added new <MenuLinks /> component

---
+++ src/components/MenuLinks.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Dropdown } from "react-bootstrap";
+
+class MenuLinks extends Component {
+  render() {
+    return;
+  }
+}
+
+export default MenuLinks;
