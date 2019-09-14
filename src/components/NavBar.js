7b5a5ff00dddefa6d3f9643d0931dc5f1f28797b
github username <github email address>
2019-09-14 01:21:20 -0400

    Review and revision of the navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,52 @@
 import React, { Component } from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Dropdown } from "react-bootstrap";
+import MenuLinks from "./MenuLinks";
 
 class NavBar extends Component {
   render() {
     return (
       <div className="navbar-container">
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect bg="dark" variant="dark">
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
+            <Navbar.Brand href="/">
+              <img
+                alt=""
+                src="https://i.imgur.com/UzrikKb.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />
+              {/* {"UIM"} */}
+            </Navbar.Brand>
+            {/* <MenuLinks /> */}
+
             <Nav className="mr-auto">
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/parent-page">Parents</Nav.Link>
               <Nav.Link href="/student-page">Students</Nav.Link>
               <Nav.Link href="/senior-page">Senior Information</Nav.Link>
               <Nav.Link href="/policies">Policies</Nav.Link>
+              <Nav.Link href="/notices">Notices</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
       </div>
+
+      // <div className="navbar-container">
+      //   <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      //     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      //     <Navbar.Collapse id="responsive-navbar-nav">
+      //       <Nav className="mr-auto">
+      //         <Nav.Link href="/">Home</Nav.Link>
+      //         <Nav.Link href="/parent-page">Parents</Nav.Link>
+      //         <Nav.Link href="/student-page">Students</Nav.Link>
+      //         <Nav.Link href="/senior-page">Senior Information</Nav.Link>
+      //         <Nav.Link href="/policies">Policies</Nav.Link>
+      //       </Nav>
+      //     </Navbar.Collapse>
+      //   </Navbar>
+      // </div>
     );
   }
 }
